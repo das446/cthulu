f516fd4b2bec6a6af4afa43a966d58353f8fcfcd
DESKTOP-7JI65JU\zero <gm498@drexel.edu>
2018-10-03 20:10:19 -0400

    demo

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    public Camera cam;
+    public float distence;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //print("I'm looking at " + hit.transform.name + " " + hit.distance);
+            // if point at something under a close enough distance
+            //hit F key to drag the box
+            if (hit.distance <= distence)
+            {
+                if (Input.GetKey(KeyCode.F)) {
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.velocity = GetComponent<Rigidbody>().velocity;
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            //print("I'm looking at nothing!");
+        }
+    }
+}
