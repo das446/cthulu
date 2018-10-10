b16139c66afd48f65839b1905f6e959be23ffe05
DESKTOP-7JI65JU\zero <gm498@drexel.edu>
2018-10-09 22:06:41 -0400

    version 0.1

---
+++ Cthulu_0.1/Assets/Scripts/Drag.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    public Camera cam;
+    public float distence;
+    public bool hold;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //print("I'm looking at " + hit.transform.name + " " + hit.distance);
+            // if point at something under a close enough distance
+            //hit F key to drag the box
+            if (hit.distance <= distence)
+            {
+                if (hold == false && hit.transform.tag == "pickable" && Input.GetKeyDown(KeyCode.F)) {
+                    hit.rigidbody.isKinematic = true;
+                    hit.transform.parent = cam.transform;
+                    hold = true;
+
+                } else if (hold == true && Input.GetKeyDown(KeyCode.F)) {
+                    hit.rigidbody.isKinematic = false;
+                    hit.transform.parent = null;
+                    hold = false;
+
+                } else if (Input.GetKey(KeyCode.F)) {
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.velocity = GetComponent<Rigidbody>().velocity;
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            //print("I'm looking at nothing!");
+        }
+    }
+}
---
+++ Cthulu_0.1/Assets/Scripts/Spawer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawer : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector3 offset;
+    public GameObject Portal;
+    void Start()
+    {
+        float rnd = Random.Range(5f, 11f);
+        StartCoroutine(respaw(rnd));
+    }
+    IEnumerator respaw(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GameObject tmp = Instantiate(Portal, gameObject.transform.position + offset, gameObject.transform.rotation);
+        tmp.transform.parent = null;
+
+    }
+
+}
