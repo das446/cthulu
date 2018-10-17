76036c40fb5b4e3bf18d1cdb26f4fe8648eb523a
DESKTOP-7JI65JU\zero <gm498@drexel.edu>
2018-10-17 19:16:17 -0400

    0.2

---
+++ Cthulu_0.2/Assets/Scripts/Scoring/ScoringController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringController : MonoBehaviour
+{
+    [SerializeField]
+    private int marketValue = 900000;
+
+    public Text valueNum;
+
+    private static ScoringController instance;
+
+    public static ScoringController Instance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        valueNum.text = "$" + marketValue;
+    }
+
+    //updates score
+    public void updateMarketValue(int valueChange)
+    {
+        marketValue += valueChange;
+        valueNum.text = "$" + marketValue;
+    }
+}
---
+++ Cthulu_0.2/Assets/Scripts/moveToRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToRoom : MonoBehaviour {
+
+    // Use this for initialization
+    public bool move = false;
+    public GameObject mainPoint;
+    private float MoveSpeed;
+    private Rigidbody rb;
+    void Start () {
+        MoveSpeed = transform.GetComponent<AI>().MoveSpeed;
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (move)
+        {
+            transform.LookAt(new Vector3(mainPoint.transform.position.x, transform.position.y, mainPoint.transform.position.z));
+            rb.AddForce((transform.forward) * MoveSpeed);
+            if (Mathf.Abs(transform.position.x - mainPoint.transform.position.x) < 5 && Mathf.Abs(transform.position.z - mainPoint.transform.position.z) < 5)
+            {
+                move = false;
+            }
+        }
+    }
+}
