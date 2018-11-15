569a9ffc17eb35525463ab9f7135a549e4c3ecca
DESKTOP-IJB5UIC\seanj <sean.jamison@rocketmail.com>
2018-11-14 21:49:17 -0500

    fixed issue with AI controller, added hand icon when interactables in reach

--- Cthulu_0.2/Assets/Scripts/AI.cs
+++ Cthulu_0.2/Assets/Scripts/AI.cs
@@ -3,22 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class AI : MonoBehaviour {
 
     AudioSource scream;
     public bool scare = false;
     public GameObject door;
-	public GameObject npcMenu;
+	public RawImage npcMenu;
 	public GameObject[] roomPoints;
     //public GameObject mainPoint;
     // Use this for initialization
     void Start ()
     {
 		roomPoints = GameObject.FindGameObjectsWithTag("roomPoint");
-		npcMenu = GameObject.FindGameObjectsWithTag("playerInteractMenu")[0];
-		npcMenu.SetActive(false);
-        scream = GetComponent<AudioSource>();
+		npcMenu = GameObject.FindGameObjectsWithTag("playerInteractMenu")[0].GetComponent<RawImage>();
+		npcMenu.enabled = false;
+		scream = GetComponent<AudioSource>();
         door = GameObject.FindGameObjectsWithTag("Door")[0];
         StartCoroutine(RoomRecognition());
     }
@@ -50,27 +51,27 @@ public class AI : MonoBehaviour {
             //print("im scared!!!!");
             gameObject.GetComponent<Inspection>().setLocation(door.transform.position);
         }
-		if(Input.GetKeyDown(KeyCode.Alpha1) && npcMenu.activeSelf)
+		if(Input.GetKeyDown(KeyCode.Alpha1) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[0].transform.position;
 			}
-		else if(Input.GetKeyDown(KeyCode.Alpha2) && npcMenu.activeSelf)
+		else if(Input.GetKeyDown(KeyCode.Alpha2) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[1].transform.position;
 			}
-		else if(Input.GetKeyDown(KeyCode.Alpha3) && npcMenu.activeSelf)
+		else if(Input.GetKeyDown(KeyCode.Alpha3) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[2].transform.position;
 			}
-		else if(Input.GetKeyDown(KeyCode.Alpha4) && npcMenu.activeSelf)
+		else if(Input.GetKeyDown(KeyCode.Alpha4) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[3].transform.position;
 			}
-		else if(Input.GetKeyDown(KeyCode.Alpha5) && npcMenu.activeSelf)
+		else if(Input.GetKeyDown(KeyCode.Alpha5) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[4].transform.position;
 			}
-		else if(Input.GetKeyDown(KeyCode.Alpha6) && npcMenu.activeSelf)
+		else if(Input.GetKeyDown(KeyCode.Alpha6) && npcMenu.enabled)
 			{
 				this.gameObject.transform.position = roomPoints[5].transform.position;
 			}
@@ -89,14 +90,14 @@ public class AI : MonoBehaviour {
         }
 		else if (other.tag == "Player" )
         {
-            npcMenu.SetActive(true);
+            npcMenu.enabled = true;
         }
     }
 	private void OnTriggerExit(Collider other)
     {
 		if (other.tag == "Player" )
         {
-            npcMenu.SetActive(false);
+            npcMenu.enabled = false;
         }
 	}
     /*
