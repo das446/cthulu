53be6d53d13a439645c60c280f4bb7f2443bf55f
Ben Filbert <bf372@drexel.edu>
2019-01-27 22:14:50 -0500

    Created script and test scene with examples of script in use. Added scene is for testing purposes only.  The Script manipulates the rigidbodies of the items in order to get them to move. Whether or not the object is heavy is a bool switch which can be flipped in the editor. Heavy objects are not quite right yet but small items work fine as of now.

---
+++ Assets/Scripts/Interactables/MoveObject.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class MoveObject :  Interactable {
+        
+        public Rigidbody rb ;
+       
+        public int grabDistance;
+
+        bool grabbed;
+
+        Transform phand;
+        Vector3 holdpos;
+        Quaternion holdrot;
+
+        Transform pPos;
+
+        public bool isHeavy;
+
+        public int throwCoefficent;
+
+         void Start()
+         {
+            grabbed = false;
+            Vector3 direction = Camera.main.transform.forward;
+         }
+
+        public  override void  Interact(Player p)
+        {
+            phand = p.hand.transform;
+        pPos = p.transform;
+        
+            GrabObject();
+        }
+
+
+        void FixedUpdate()
+        {
+             Debug.Log("Grab State?: "+grabbed.ToString());
+            if (grabbed && !isHeavy)
+            {
+                holdpos = phand.position;
+                holdrot = phand.rotation;
+                //rb.AddForceAtPosition(direction,holdpos,ForceMode.Acceleration );
+                rb.MovePosition(holdpos);
+                rb.MoveRotation(holdrot);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DropObject();
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    ThrowObject();
+                }
+            }
+
+        }
+//* 
+void Update()
+{
+    
+
+            if (grabbed && isHeavy)
+            {
+                
+                //rb.AddForce(new Vector3(0,-1000,0));
+                holdpos = phand.position;
+                holdrot = phand.rotation;
+                //rb.AddForceAtPosition(direction,holdpos,ForceMode.Acceleration );
+                //rb.MovePosition(Vector3.MoveTowards(transform.position,new Vector3(holdpos.x,-1,holdpos.z),throwCoefficent));
+                //my_forward.y = 0;
+                //transform.position += my_forward.normalized * moveSpeed * Time.deltaTime;
+
+            if ((holdpos-transform.position).x > grabDistance || (holdpos-transform.position).y > grabDistance || (holdpos-transform.position).z > grabDistance)
+            {
+                Debug.Log("Max distance from object, object dropped");
+                DropObject();
+            }
+
+               rb.AddForce((holdpos-transform.position + new Vector3(0,-1,0))*throwCoefficent);
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DropObject();
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    ThrowObject();
+                }
+            }
+
+
+        }
+// */
+        void DropObject()
+        {
+            Debug.Log("Dropped Object");
+            grabbed = false;
+            rb.useGravity = true;
+        }
+
+        void ThrowObject()
+        {
+            Debug.Log("Threw Object");
+            grabbed = false;
+            rb.useGravity = true;
+            
+            rb.AddRelativeForce(new Vector3(0,0, throwCoefficent),ForceMode.Impulse);
+        }
+
+        void GrabObject()
+        {
+            grabbed = true;
+            rb.useGravity = false;
+        }
+
+
+    }
