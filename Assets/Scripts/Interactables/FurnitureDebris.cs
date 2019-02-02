52deb669c720b24c70d9a761dc42deeaf49042e1
nintendavid26 <nintendavid26@aol.com>
2019-02-02 17:32:32 -0500

    added broom

--- Assets/Scripts/Interactables/FurnitureDebris.cs
+++ Assets/Scripts/Interactables/FurnitureDebris.cs
@@ -4,17 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FurnitureDebris : Interactable, IEvaluated {
+
+    [SerializeField] GameObject sweep;
+    [SerializeField] float delay = 3;
+
     public float Evaluate(Npc npc, Room r) {
         return -20;
     }
 
     public override void Interact(Player p) {
-        CleanUp();
+        StartCoroutine(CleanUp(p));
     }
 
-    private void CleanUp() {
-        //Play Sweep animation
-
+    IEnumerator CleanUp(Player p) {
+        p.Lock();
+        GameObject s = Instantiate(sweep, transform.position, p.transform.rotation);
+        s.transform.Rotate(0,90,0);
+        yield return new WaitForSeconds(delay);
+        p.Unlock();
+        Destroy(s);
         Destroy(gameObject);
     }
 }
