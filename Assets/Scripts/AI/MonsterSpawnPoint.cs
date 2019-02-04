fe05e7f319843306684f8a0c6280b5a4167db12d
nintendavid26 <nintendavid26@aol.com>
2019-02-03 22:12:21 -0500

    Added portal to scene

--- Assets/Scripts/AI/MonsterSpawnPoint.cs
+++ Assets/Scripts/AI/MonsterSpawnPoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MonsterSpawnPoint : MonoBehaviour {
 
-    Monster curMonster;
+    [SerializeField] Monster curMonster;
     // Start is called before the first frame update
     void Start() {
 
--- Assets/Scripts/Monsters/Tentacle.cs
+++ Assets/Scripts/Monsters/Tentacle.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tentacle : Monster, ICanHold {
     [SerializeField] IPickUpable held;
+    [SerializeField] float range;
+    [SerializeField] BoxCollider hitbox;
 
     public Transform Hand =>
         throw new System.NotImplementedException();
@@ -15,14 +18,10 @@ public class Tentacle : Monster, ICanHold {
         return held;
     }
 
-
-
     public override void FurnitureContact(Furniture furniture) {
-        GetHit((int)furniture.weight);
+        GetHit((int) furniture.weight);
         //Randomly grab it
     }
-
-    
 
     public Vector3 GetThrowDir() {
         //throw at NPC or player
@@ -30,7 +29,18 @@ public class Tentacle : Monster, ICanHold {
     }
 
     public override void OnSpawn() {
-        throw new System.NotImplementedException();
+        SetCollider();
+    }
+
+    void SetCollider() {
+        Vector3 center = hitbox.center;
+        Vector3 bounds = hitbox.size;
+        center.x = range / 2;
+        bounds.x = range;
+        bounds.z = range;
+        hitbox.center = center;
+        hitbox.size = bounds;
+
     }
 
     public void PickUp(IPickUpable pickUpable) {
@@ -41,6 +51,14 @@ public class Tentacle : Monster, ICanHold {
     public void Release(IPickUpable pickUpable) {
         held.Release(this);
         held = null;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject);
+        Npc npc = other.GetComponent<Npc>();
+        if (npc != null) {
+            npc.GetPickedUp(this);
+        }
     }
 
 }
