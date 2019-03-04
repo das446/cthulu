36d5606bdcd28c65f14cb4988894253a61509495
louislee95 <ll579@drexel.edu>
2019-03-04 13:23:59 -0500

    Added a Plugins folder and NSubstitute.dll in that folder to help with Unit Testing

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -4,18 +4,37 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEditor;
-using Cthulu.MonsterSpawner;
+using UnityEditor.SceneManagement;
 
 namespace Tests
 {
-    public class NewTestScript
+    public class NewTestScript : MonoBehaviour
     {
 
+        public GameObject monsterSpawnController = GameObject.Find("MonsterSpawnController");
         // A Test behaves as an ordinary method
         [Test]
         public void NewTestScriptSimplePasses()
         {
+            bool isActive = false;
+            Assert.AreEqual(false, isActive);
             // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void CatchingErrors()
+        {
+            GameObject gameobject = new GameObject("test");
+            Assert.Throws<MissingComponentException>
+            (
+                () => gameobject.GetComponent<Rigidbody>().velocity = Vector3.one
+            );
+        }
+
+        [SetUp]
+        public void ResetScene()
+        {
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -25,13 +44,14 @@ namespace Tests
         public IEnumerator InstantiateTentacleFromMonsterSpawner()
         {
             var tentaclePrefab = Resources.Load("Tests/Tentacle");
-            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             tentacleSpawner.Construct(tentaclePrefab, 0, 1);
 
             yield return null;
 
             var spawnedTentacle = GameObject.Find("Tentacle");
-            var prefabOfSpawnedTentacle = PrefabUtility.GetPrefabParent(spawnedTentacle);
+            var prefabOfSpawnedTentacle = PrefabUtility.GetCorrespondingObjectFromSource(spawnedTentacle);
+            //var prefabOfSpawnedTentacle = PrefabUtility.GetPrefabParent(spawnedTentacle);
             Assert.AreEqual(tentaclePrefab, prefabOfSpawnedTentacle);
         }
 
@@ -39,13 +59,16 @@ namespace Tests
         public IEnumerator InstantiatePortalFromMonsterSpawner()
         {
             var portalPrefab = Resources.Load("Tests/Portal");
-            var portalSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var portalSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             portalSpawner.Construct(portalPrefab, 0, 1);
 
             yield return null;
 
             var spawnedPortal = GameObject.Find("Portal");
-            var prefabOfSpawnedPortal = PrefabUtility.GetPrefabParent(spawnedPortal);
+            var prefabOfSpawnedPortal = PrefabUtility.GetCorrespondingObjectFromSource(spawnedPortal);            
+            //The GetPrefabParent is obsolete
+            //var prefabOfSpawnedPortal = PrefabUtility.GetPrefabParent(spawnedPortal);
+
             Assert.AreEqual(portalPrefab, prefabOfSpawnedPortal);
         }
 
@@ -53,9 +76,9 @@ namespace Tests
         public IEnumerator InstantiateGameObjectAtRandomPosition()
         {
             var portalPrefab = Resources.Load("Tests/Portal");
-            var portalSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var portalSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             portalSpawner.Construct(portalPrefab, 0, 1);
-            var random = new Vector3(0,0,(-10, 10));
+            var random = new Vector3(0,0,Random.Range(-10, 10));
             portalSpawner.Random = random;
             var spawnedPortal = GameObject.Find("Portal");
             var expectedPortalPosition = new Vector3(0,0,-1);
@@ -64,7 +87,7 @@ namespace Tests
             yield return null;
 
             var tentaclePrefab = Resources.Load("Tests/Tentacle");
-            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             tentacleSpawner.Construct(tentaclePrefab, 0, 1);
             tentacleSpawner.Random = random;
             var spawnedTentacle = GameObject.Find("Tentacle");
@@ -76,41 +99,70 @@ namespace Tests
         public IEnumerator InstantiationsOccurOnAnInterval()
         {
             var portalPrefab = Resources.Load("Tests/Portal");
-            var portalSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var portalSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             portalSpawner.Construct(portalPrefab,1,1);
 
             var tentaclePrefab = Resources.Load("Tests/Tentacle");
-            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawner>();
+            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawnController>();
             tentacleSpawner.Construct(tentacleSpawner,1,1);
 
             var spawnedPortal = GameObject.Find("Portal");
             var spawnedTentacle = GameObject.Find("Tentacle");
 
             yield return new WaitForSeconds(0.75f);
+            Assert.IsNull(spawnedTentacle);
             Assert.IsNull(spawnedPortal);
         }
+		
+        [UnityTest]
+        public IEnumerator InstantiatesGameObjectsAtRandomPositionOnCircularBoundary()
+        {
+            var random = NSubstitute.Substitute.For<System.Random>();
+            random.Next(Arg.Any<int>(), Arg.Any<int>()).Returns(270);
 
+            var portalPrefab = Resources.Load("Tests/Portal");
+            var portalSpawner = new GameObject().AddComponent<MonsterSpawnController>();
+            portalSpawner.Construct(portalPrefab, 0, 1);
+            portalSpawner.Random = random;
+            var spawnedPortal = GameObject.Find("Portal");
+            var expectedPortalPosition = new Vector3(0,0,-1);
+            Assert.AreEqual(expectedPortalPosition, spawnedPortal.transform.position);
+
+            var tentaclePrefab = Resources.Load("Tests/Tentacle");
+            var tentacleSpawner = new GameObject().AddComponent<MonsterSpawnController>();
+            tentacleSpawner.Construct(tentaclePrefab, 0, 1);
+            tentacleSpawner.Random = random;
+            var spawnedTentacle = GameObject.Find("Tentacle");
+            var expectedTentaclePosition = new Vector3(0,0,-1);
+            Assert.AreEqual(expectedTentaclePosition, spawnedTentacle.transform.position);
+            yield return null;
+        }
+		
         [UnityTest]
         public IEnumerator MoneyGoesUp()
         {
+            yield return null;
 
         }
 
         [UnityTest]
         public IEnumerator InstantiateNPC()
         {
+            yield return null;
 
         }
 
         [UnityTest]
         public IEnumerator ActionsCanBePerformed()
         {
+            yield return null;
 
         }
 
         [UnityTest]
         public IEnumerator CanScareNPCS()
         {
+            yield return null;
 
         }
 
@@ -124,7 +176,7 @@ namespace Tests
                     Object.Destroy(gameObject);
                 }
 
-                foreach (var gameObject in Object.Find("Portal"))
+                foreach (var gameObject in Object.FindObjectsOfType<MonsterSpawnController>())
                 {
                     Object.Destroy(gameObject);
                 }
