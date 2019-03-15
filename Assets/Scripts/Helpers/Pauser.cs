75e7ad031fc5194dc997ecef58c147205fc4501c
nintendavid26 <nintendavid26@aol.com>
2019-03-15 14:17:01 -0400

    added pause menu

--- Assets/Scripts/Helpers/Pauser.cs
+++ Assets/Scripts/Helpers/Pauser.cs
@@ -2,40 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Pauser : MonoBehaviour
-{
+public class Pauser : MonoBehaviour {
     bool paused = false;
     [SerializeField] GameObject menu;
     public GameObject cam;
 
+    public static event Action OnPause;
+    public static event Action OnUnPause;
+
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.P)){
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             paused = !paused;
 
-            if(paused){
+            if (paused) {
                 Pause();
-            }
-            else{
+            } else {
                 UnPause();
             }
 
         }
     }
 
-    private void UnPause()
-    {
+    public void UnPause() {
         Time.timeScale = 1;
         Player.singleton.Unlock();
-        cam.SetActive(false);
+        menu.SetActive(false);
+        OnUnPause();
     }
 
-    private void Pause()
-    {
+    private void Pause() {
         Time.timeScale = 0;
         Player.singleton.Lock();
-        cam.SetActive(true);
+        menu.SetActive(true);
+        OnPause();
+    }
+
+    public void GoMain(){
+        SceneManager.LoadScene("StartMenu");
+        Player.ResetStatics();
     }
 }
